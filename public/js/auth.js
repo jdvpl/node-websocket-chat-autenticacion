6b791e2536148250a74b5331a3826795240a7174
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-04-16 01:14:31 -0500

    login con el de la db

---
+++ public/js/auth.js
@@ -0,0 +1,72 @@
+// /referencias
+const formulario=document.querySelector('form');
+
+
+const url = window.location.hostname.includes("localhost")
+    ? "http://localhost:4000/api/auth/"
+    : "https://jdvpl-rest-server-node.herokuapp.com/api/auth/";
+
+
+
+formulario.addEventListener('submit',ev => {
+  ev.preventDefault();
+
+  const formData={};
+  
+  for (let el of formulario) {
+    if(el.name.length > 0) {
+       formData[el.name] = el.value;
+    }
+  }
+  
+  fetch(url+"login",{
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  }).then(res => res.json())
+  .then(({msg,token})=>{
+    if(msg){
+      return console.log(msg);
+    }
+    localStorage.setItem('token', token);
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+
+})
+
+function handleCredentialResponse(response) {
+  
+  //  const tokenGoogle = response.credential;
+  //  console.log(tokenGoogle)
+  const body = { id_token: response.credential };
+  fetch(url+"google", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(body),
+  })
+    .then((resp) => resp.json())
+    .then((resp) => {
+      const boton = (document.getElementById(
+        "google_signout"
+      ).style.display = "block");
+      localStorage.setItem('token',resp.token);
+      localStorage.setItem("email", resp.usuario.email);
+    })
+    .catch(console.warn());
+}
+
+const button = document.getElementById("google_signout");
+
+button.onclick = () => {
+  google.accounts.id.disableAutoSelect();
+  google.accounts.id.revoke(localStorage.getItem("email"), (done) => {
+    localStorage.clear();
+    location.reload();
+  });
+};
